46e756d3a3a813d11593c62b458796cf4b8f9b3c
Brendan Conrad <brconrad@symitar.com>
2018-11-19 10:05:25 -0800

    added test for empty result when no hashtag match is found

--- test/src/index.spec.js
+++ test/src/index.spec.js
@@ -4,12 +4,18 @@ var expect = require('chai').expect;
 var nodeIG = require('../../index');
 
 describe('node-ig', function() {
-  it('return media by the hashtag "symitar"', function() {
-    nodeIG.searchByHashtag('symitar').then(function(res) {
+  it('return media by the hashtag "money"', function() {
+    nodeIG.searchByHashtag('money').then(function(res) {
       const igData = res.data;
       expect(igData).to.be.an('array').that.is.not.empty;
       const media = igData[0];
       expect(media.user.username).to.be.a('string').that.is.not.empty;
+    });
+  });
+  it('return an empty result when searching by the hashtag "emptyresult"', function() {
+    nodeIG.searchByHashtag('emptyresult').then(function(res) {
+      const igData = res.data;
+      expect(igData).to.be.an('array').that.is.empty;
     });
   });
 });
